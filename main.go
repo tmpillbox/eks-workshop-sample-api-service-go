86c3093ddf3a4c4280ded54ac5d2ff78c67b9dbf
pillbox <tmpillbox@users.noreply.github.com>
2019-11-14 17:09:35 -0600

    Change the message text

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "¡Hola!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
